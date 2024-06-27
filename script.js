544c372e32164461ed68e4a9c3f8429cc9c11f9e
Seth Donelson <seth.donelson@gmail.com>
2024-06-27 16:50:57 -0400

    animation and keyboard

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ import { WORDS } from "./words.js"; //attaches js file with word options
 
 
 const NUMBER_OF_GUESSES = 6  //all caps since value is known
-let guessesRemaining = NUMBER_OF_GUESSES
+let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];  //each letter gets displayed as they're entered
 let nextLetter = 0;  //index of the array
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]  //generate random num 0-1 rounded down, times array index 0-5
 console.log(rightGuessString)   //can inspect correct word
 
 
+//initialize game board
 function initBoard(){
     let board = document.getElementById('game-board');
 
@@ -35,6 +36,8 @@ function initBoard(){
 initBoard()  //refeshes board on page load
 
 
+
+//accept user input from keyboard
 document.addEventListener("keyup", (e) => {
 
     if (guessesRemaining === 0) {   //no response if all boxes are filled
@@ -70,6 +73,7 @@ function insertLetter (pressedKey) {
 
     let row = document.getElementsByClassName('letter-row')[6 - guessesRemaining]  //gives current empty row
     let box = row.children[nextLetter]   //moves to next empty box
+    animateCSS(box, "pulse")   //pulse box before it's filled
     box.textContent = pressedKey  //add content to box
     box.classList.add("filled-box")   //formats the box
     currentGuess.push(pressedKey) //adds key to end of word guess in array
@@ -96,12 +100,12 @@ function checkGuess () {
     }
 
     if (guessString.length != 5) {  //checks for 5 letters before enter
-        alert("Not enough letters!")
+        toaster.error("Not enough letters!")
         return
     }
 
     if(!WORDS.includes(guessString)){  //looks for guessString in possible list of words
-        alert("word not in list!")
+        toastr.error("word not in list!")
         return
     }
 
@@ -129,14 +133,83 @@ function checkGuess () {
         
         let delay = 250 * i
         setTimeout(() => {    //puts shading in the box
+            animateCSS(box, 'flipInX')  //flip the box befor color add
             box.style.backgroundColor = letterColor
-            shadeKeyBoard(letter, letterColor)
+            shadeKeyBoard(letter, letterColor) //shades the letter, function listed below
         }, delay)
     }
 
+    if (guessString === rightGuessString) {   //conditional for winning the game
+        toastr.success("You guessed right! Game over!")
+        guessesRemaining = 0  //forces game to end
+        return
+    } else {
+        guessesRemaining -= 1;  //removes 1 from guess count if wrong
+        currentGuess = []; //moves to next row with empty array
+        nextLetter = 0;  //reset to beginning
 
-
+        if (guessRemaining === 0) {  //indicates last guess
+            toastr.error("You've run out of guesses! Game over!")
+            toastr.info(`The right word was: "${rightGuessString}"`)  //displays the correct word, game over
+        }
+    }
 
 }
 
-if (guessString)
+function shadeKeyBoard(letter, color) {
+    for (const elem of document.getElementsByClassName("keyboard-button")) { //for every key on keyboard, does letter match color
+        if (elem.textContent === letter) {
+            let oldColor = elem.style.backgroundColor  //get current color of key
+            if (oldColor === 'green'){  //keeps the key green
+                return
+            }
+
+            if (oldColor === 'yellow' && color !== 'green'){  //if yellow, allow key to turn green
+                return
+            }
+
+            elem.style.backgroundColor = color  //
+            break
+        }
+    }
+}
+
+//creates input for on-screen keyboard
+document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+    const target = e.target  //identify what was clicked
+    
+    //not a keyboard button, exit function
+    if (!target.classList.contains("keyboard-button")) {
+        return
+    }
+    //sets key = to the text on the keyboard button
+    let key = target.textContent
+
+    if (key === "Del") {
+        key = "Backspace"
+    } 
+    //mimics the physical keyboard with the virtual board and allows for clicking
+    document.dispatchEvent(new KeyboardEvent("keyup", {'key': key}))
+})
+
+
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+      const animationName = `${prefix}${animation}`;
+      // const node = document.querySelector(element);
+      const node = element
+      node.style.setProperty('--animate-duration', '0.3s');
+      
+      node.classList.add(`${prefix}animated`, animationName);
+  
+      // When the animation ends, we clean the classes and resolve the Promise
+      function handleAnimationEnd(event) {
+        event.stopPropagation();
+        node.classList.remove(`${prefix}animated`, animationName);
+        resolve('Animation ended');
+      }
+  
+      node.addEventListener('animationend', handleAnimationEnd, {once: true});
+  });
