4514085a6d2b4594b29f39b5581b5adc5b869f0c
Seth Donelson <155389209+SethDonelson@users.noreply.github.com>
2024-09-16 21:15:27 -0400

    updated comments

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//code will have several local variables that help to simplify the code with a longer script
-
 
 import { WORDS } from "./words.js"; //attaches js file with word options
 
@@ -7,16 +5,16 @@ import { WORDS } from "./words.js"; //attaches js file with word options
 const NUMBER_OF_GUESSES = 6  //all caps since value is known
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];  //each letter gets displayed as they're entered
-let nextLetter = 0;  //index of the array
-let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]  //generate random num 0-1 rounded down, times array index 0-5
-console.log(rightGuessString)   //can inspect correct word
+let nextLetter = 0; 
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)] 
+console.log(rightGuessString)  
 
 
 //initialize game board
 function initBoard(){
     let board = document.getElementById('game-board');
 
-    //creates 6 rows, adds to the board container
+    //creates 6 rows
     for (let i = 0; i < NUMBER_OF_GUESSES; i++){
         let row = document.createElement('div')
         row.className = "letter-row"
@@ -33,14 +31,14 @@ function initBoard(){
     
 }
 
-initBoard()  //refeshes board on page load
+initBoard()  
 
 
 
 //accept user input from keyboard
 document.addEventListener("keyup", (e) => {
 
-    if (guessesRemaining === 0) {   //no response if all boxes are filled
+    if (guessesRemaining === 0) { 
         return
     }
  
@@ -50,16 +48,17 @@ document.addEventListener("keyup", (e) => {
         return
     }
 
-    if (pressedKey === "Enter") {  //run checkGuess if Enter is clicked
+    if (pressedKey === "Enter") { 
         checkGuess()
         return
     }
 
-    let found = pressedKey.match(/[a-z]/gi)   //regular expression, allows for matching of a-z
-    if (!found || found.length > 1) {  //if not found or more than 1 key, then exit/do nothing
+    //matches a-z, ignore invalid letter
+    let found = pressedKey.match(/[a-z]/gi)  
+    if (!found || found.length > 1) {  
         return
     } else {
-        insertLetter(pressedKey) //if single key, then run insertLetter
+        insertLetter(pressedKey) 
     }
 })
 
@@ -71,40 +70,40 @@ function insertLetter (pressedKey) {
     }
     pressedKey = pressedKey.toLowerCase()
 
-    let row = document.getElementsByClassName('letter-row')[6 - guessesRemaining]  //gives current empty row
-    let box = row.children[nextLetter]   //moves to next empty box
-    animateCSS(box, "pulse")   //pulse box before it's filled
-    box.textContent = pressedKey  //add content to box
-    box.classList.add("filled-box")   //formats the box
-    currentGuess.push(pressedKey) //adds key to end of word guess in array
-    nextLetter += 1   //adds 1 to the index/counter
+    let row = document.getElementsByClassName('letter-row')[6 - guessesRemaining]  
+    let box = row.children[nextLetter]   
+    animateCSS(box, "pulse")   
+    box.textContent = pressedKey  
+    box.classList.add("filled-box")   
+    currentGuess.push(pressedKey) 
+    nextLetter += 1   
 }
 
 //finds correct row and box, removes the key and returns to emptyspace
 function deleteLetter(){
-    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]  //gives current row
-    let box = row.children[nextLetter - 1] //gives most current filled box
-    box.textContent = ""   //sets to empty box
-    box.classList.remove("filled-box")  //removes styling
-    currentGuess.pop()   //removes last key of the array/word
-    nextLetter -= 1  //takes one away from the counter
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining] 
+    let box = row.children[nextLetter - 1] 
+    box.textContent = ""   
+    box.classList.remove("filled-box")  
+    currentGuess.pop()   
+    nextLetter -= 1  
 }
 
 function checkGuess () {
     let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
     let guessString = ''
-    let rightGuess = Array.from(rightGuessString)  //pulls word from random word list, const at the top
+    let rightGuess = Array.from(rightGuessString)  
 
     for (const val of currentGuess) {   //loops through array and adds to the string
         guessString  += val 
     }
 
-    if (guessString.length != 5) {  //checks for 5 letters before enter
+    if (guessString.length != 5) {  
         toaster.error("Not enough letters!")
         return
     }
 
-    if(!WORDS.includes(guessString)){  //looks for guessString in possible list of words
+    if(!WORDS.includes(guessString)){  
         toastr.error("word not in list!")
         return
     }
@@ -124,33 +123,36 @@ function checkGuess () {
             //letter is in word, correct position/index
             if (currentGuess[i] === rightGuess[i]) {
                 letterColor = 'green'
-            } else {   //letter is in the word, but not position/index
+            } else {  
                 letterColor = 'yellow'
             }
 
             rightGuess[letterPosition] = "#"
         }
         
+        //shades box and letter
         let delay = 250 * i
-        setTimeout(() => {    //puts shading in the box
-            animateCSS(box, 'flipInX')  //flip the box befor color add
+        setTimeout(() => {    
+            animateCSS(box, 'flipInX')  
             box.style.backgroundColor = letterColor
-            shadeKeyBoard(letter, letterColor) //shades the letter, function listed below
+            shadeKeyBoard(letter, letterColor) 
         }, delay)
     }
 
-    if (guessString === rightGuessString) {   //conditional for winning the game
+    //game winning condition
+    if (guessString === rightGuessString) {   
         toastr.success("You guessed right! Game over!")
-        guessesRemaining = 0  //forces game to end
+        guessesRemaining = 0  
         return
     } else {
-        guessesRemaining -= 1;  //removes 1 from guess count if wrong
-        currentGuess = []; //moves to next row with empty array
-        nextLetter = 0;  //reset to beginning
+        //removes a guess
+        guessesRemaining -= 1;  
+        currentGuess = []; 
+        nextLetter = 0;  
 
         if (guessRemaining === 0) {  //indicates last guess
             toastr.error("You've run out of guesses! Game over!")
-            toastr.info(`The right word was: "${rightGuessString}"`)  //displays the correct word, game over
+            toastr.info(`The right word was: "${rightGuessString}"`)  
         }
     }
 
@@ -168,7 +170,7 @@ function shadeKeyBoard(letter, color) {
                 return
             }
 
-            elem.style.backgroundColor = color  //
+            elem.style.backgroundColor = color  
             break
         }
     }
@@ -193,18 +195,16 @@ document.getElementById("keyboard-cont").addEventListener("click", (e) => {
 })
 
 
-
+//animation setup
 const animateCSS = (element, animation, prefix = 'animate__') =>
-    // We create a Promise and return it
     new Promise((resolve, reject) => {
       const animationName = `${prefix}${animation}`;
-      // const node = document.querySelector(element);
       const node = element
       node.style.setProperty('--animate-duration', '0.3s');
       
       node.classList.add(`${prefix}animated`, animationName);
   
-      // When the animation ends, we clean the classes and resolve the Promise
+      // end the animation
       function handleAnimationEnd(event) {
         event.stopPropagation();
         node.classList.remove(`${prefix}animated`, animationName);
